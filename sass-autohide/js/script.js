b6ffd0c8eb40047449e3e29ceb472d2a9eca1aee
Eddy Mio <you@mip.com>
2020-06-20 08:11:47 +0200

    Variabilization

--- sass-autohide/js/script.js
+++ sass-autohide/js/script.js
@@ -1,8 +1,21 @@
-const lock = document.getElementById("lock");
-const navigation = document.getElementById("navigation");
+const lock = document.getElementById('lock');
+const navigation = document.getElementById('navigation');
+let isLocked = false;
 
-lock.addEventListener("click", function(){
-    navigation.classList.remove("nav--autohide");
-    navigation.classList.add("nav--locked");
+lock.addEventListener('click', function(){
+
+    if (isLocked)
+    {
+        navigation.classList.remove('nav--locked');
+        navigation.classList.add('nav--autohide');
+        this.innerHTML = 'LOCK';
+        isLocked = false;
+    }
+    else {
+        navigation.classList.remove('nav--autohide');
+        navigation.classList.add('nav--locked');
+        this.innerHTML = 'UNLOCK';
+        isLocked = true;
+    }
 });
 
